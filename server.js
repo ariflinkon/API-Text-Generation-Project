e9f809f030b74761a7d887a350f16dd96e163785
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-08-02 04:05:41 +0600

    updated the server file and added Mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { MongoClient } = require('mongodb');
+/* const MongoClient = require('mongodb').MongoClient; */
 const { HfInference } = require("@huggingface/inference");
 const path = require('path');
 
@@ -8,12 +10,27 @@ const port = 3000;
 
 const inference = new HfInference("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");
 
+// MongoDB connection URL and database name
+const mongoUrl = 'mongodb+srv://ariflinkon:GVrC7PlBzYrtxzn8@cluster0.ptir0ud.mongodb.net/';
+const dbName = 'textgenerationdb';
+
+let db;
+
+// Connect to MongoDB
+MongoClient.connect(mongoUrl, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log('Connected to MongoDB');
+    db = client.db(dbName);
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB', error);
+  });
+
 app.use(bodyParser.json());
-/* app.use(express.static('public')); */
 
 // Serve the HTML file for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, /* 'public',  */'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.post('/chat', async (req, res) => {
@@ -28,9 +45,18 @@ app.post('/chat', async (req, res) => {
     responseText += chunk.choices[0]?.delta?.content || "";
   }
 
+  // Store user message and AI response in MongoDB
+  const chatCollection = db.collection('chats');
+  await chatCollection.insertOne({
+    user: 'You',
+    message: userInput,
+    response: responseText,
+    timestamp: new Date(),
+  });
+
   res.json({ response: responseText });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running`);
 });
